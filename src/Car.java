8ce6961f5ae4288813199dc764827f6dbe36ea99
PrincessBaloyi <lulamani.baloyi@gmail.com>
2020-08-05 03:42:25 +0200

    created Vehicle interface, Car class, Driver class and Passenger class

---
+++ src/Car.java
@@ -0,0 +1,40 @@
+public class Car implements Vehicle {
+    
+    //instance variables
+    String numberPlate;
+    String color;
+    String model;
+    String type;
+    double base_rate;
+    //constructors
+    Car(String numberPlate, String color, String model, String type) {
+        this.numberPlate = numberPlate;
+        this.color = color;
+        this.model = model;
+        this.type = type;
+    }
+
+    //methods
+    public String getNumberPlate() {
+        return this.numberPlate;
+    }
+
+    public double getBaseRate() {
+        return this.base_rate;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+    public String getModel() {
+        return this.model;
+    }
+
+    public String getVehicleType() {
+        return this.type;
+    }
+
+    public String toString() {
+        return this.model + " car of " + this.color + " color, with number plate: " + this.numberPlate;
+    }
+}
